b278f88eedaa456a3323be1b9f6effbe50941b4e
Milimeter <razaqolad300@gmail.com>
2023-06-11 00:45:20 +0100

    Constant file refactored to provide class access

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -2,6 +2,6 @@ import 'package:logger/logger.dart';
 
 class Constants {
   static final Logger logger = Logger();
-  static const String apiKey = '';
+  static String apiKey = '';
   static const String baseurl = 'https://deep-index.moralis.io/api/v2';
 }
